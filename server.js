fa0c51e3e74c61a441e8c137087addf478c09c26
rhythm1920 <58517101+rhythm1920@users.noreply.github.com>
2020-09-12 23:20:17 +0530

    added /api/whoami endpoint"

--- server.js
+++ server.js
@@ -24,9 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.use('/api/whoami',(req,res)=>{
+res.json({"ipaddress":req.ip,"language":req.headers["accept-language"],"software":req.headers["user-agent"]});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log('View the app here : http://localhost:' + listener.address().port);
 });
