58c9ea5b3d93f83b8da8a7dfb76881fe2bea394d
Rhythm <58517101+rhythm1920@users.noreply.github.com>
2021-01-19 22:47:49 +0530

    added more frontend functionality

--- server.js
+++ server.js
@@ -2,34 +2,39 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-app.use('/api/whoami',(req,res)=>{
-res.json({"ipaddress":req.ip,"language":req.headers["accept-language"],"software":req.headers["user-agent"]});
+app.use("/api/whoami", (req, res) => {
+  res.json({
+    ip: req.ip,
+    lang: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  console.log('View the app here : http://localhost:' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
+  console.log(
+    "View the app here : http://localhost:" + listener.address().port
+  );
 });
